515d2e75d713e486707bb9c3b34554d68ed03966
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-07-22 17:06:54 -0300

    Ajuste no controller do projeto

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -1,7 +1,27 @@
+require 'httparty'
+
 class EmpresasController < ApplicationController
   def index
+    integrador = Integrador.new(ENV["RAZONET_TOKEN"])
+    resposta = integrador.buscar_empresas
+
+    if resposta.code == 200
+      @empresas = resposta.parsed_response["companies"] || []
+    else
+      @empresas = []
+    end
   end
 
   def integrar
+    integrador = Integrador.new(ENV["RAZONET_TOKEN"])
+
+    dados = {
+      cnpj: params[:cnpj],
+      note: params[:note],
+    }
+
+    resposta = integrador.integrar_empresa(dados)
+
+    redirect_to root_path(status: resposta.code)
   end
 end
